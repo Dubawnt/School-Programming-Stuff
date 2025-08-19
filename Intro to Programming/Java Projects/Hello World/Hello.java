fb3272bf4a205f526ff5bdd8a75261b149ecf9d6
Dubawnt <dubawnt@gmail.com>
2025-08-18 23:21:47 -0400

    Added Hello.java
    First assignment in Intro to Programming class 8/18/25

---
+++ Intro to Programming/Java Projects/Hello World/Hello.java
@@ -0,0 +1,10 @@
+package prog;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to java programming");
+	}
+
+}
