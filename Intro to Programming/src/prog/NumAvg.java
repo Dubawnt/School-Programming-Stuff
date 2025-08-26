cb8d48db51594f1a6bdc75545a7a9adbf0a8e415
Dubawnt <dubawnt@gmail.com>
2025-08-26 10:03:28 -0400

    Add AreaCircle and NumAvg Java programs
    Introduced AreaCircle.java to calculate the area of a circle based on user input, and NumAvg.java to compute the sum and average of five predefined numbers. Includes corresponding compiled class files.

---
+++ Intro to Programming/src/prog/NumAvg.java
@@ -0,0 +1,27 @@
+package prog;
+//program to display sum and take the average of 5 numbers
+public class NumAvg {
+
+	public static void main(String[] args) {
+		int n1;
+		int n2;
+		int n3;
+		int n4;
+		int n5;
+		int sum; //declare output
+		int avg; 
+		
+		n1=2; //initialize variables
+		n2=3;
+		n3=4;
+		n4=5;
+		n5=6;
+		
+		sum= n1+n2+n3+n4+n5;
+		avg= sum/5;
+		
+		//Display Result
+		System.out.println("The sum of the five numbers is " + sum + " and the average is: " +  avg);
+	}
+
+}
