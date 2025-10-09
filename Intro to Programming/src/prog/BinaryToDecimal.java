d414b59812177f7e935b475b9eb9a799fadb83fe
Dubawnt <dubawnt@gmail.com>
2025-10-09 10:32:11 -0400

    added BinaryToDecimal program to convert 4-digit binary strings to decimal values

---
+++ Intro to Programming/src/prog/BinaryToDecimal.java
@@ -0,0 +1,40 @@
+package prog;
+
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+
+        //User Input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please input a 4 digit binary string:");
+        String userInput = input.nextLine();
+        input.close();
+
+        //Input Validation
+        int length = userInput.length();
+        if (length != 4) {
+            System.out.println("This string is not 4 digits long.");
+            System.exit(0);
+        }
+
+        //conversion to decimal
+        int decimal = 0;
+        if (userInput.charAt(0) == '1') {
+            decimal = decimal + 8;
+        }
+        if (userInput.charAt(1) == '1') {
+            decimal = decimal + 4;
+        }
+        if (userInput.charAt(2) == '1') {
+            decimal = decimal + 2;
+        }
+        if (userInput.charAt(3) == '1') {
+            decimal = decimal + 1;
+        }
+
+        //display
+        System.out.println("The decimal output of " + userInput + " is " + decimal);
+
+    }
+}
